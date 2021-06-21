128481a3435afac74e88c0511cc465d99d24e5a2
silasm01 <74141641+silasm01@users.noreply.github.com>
2021-06-21 18:25:41 +0200

    Add files via upload

---
+++ Website.py
@@ -0,0 +1,22 @@
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def start():
+    return redirect(url_for("home"))
+
+
+@app.route("/home/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/login/")
+def login():
+    return render_template("login.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
